d92e424915864d66d0da5606cb4764a8783a18b9
ibrahimlawal <ibrahim@lawal.me>
2015-12-31 11:26:44 +0100

    tests passed

--- sample.js
+++ sample.js
@@ -2,7 +2,12 @@ var Luhn = require('./luhn-string');
 var assert = require('assert');
 var chars='UYSHO*(EKJDGuihsdk8aBAzZ', len;
 
+len = Math.floor(Math.random() * (18 - 7 + 1)) + 7;
+console.log(Luhn.cryptoRandomString(len, chars));
+
+var str = Luhn.cryptoRandomAsciiString(len);
+
 for(var i=0;i<10;i++){
 len = Math.floor(Math.random() * (18 - 7 + 1)) + 7;
-console.log(Luhn.random(len));
+  console.log(Luhn.random(len));
 }
